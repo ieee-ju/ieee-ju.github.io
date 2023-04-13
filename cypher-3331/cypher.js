f85dd4f2c8ff97df20eeff60986717debc207d64
Subhadip De <subhadipde.habra@gmail.com>
2023-04-13 23:39:00 +0530

    final page done

--- cypher-3331/cypher.js
+++ cypher-3331/cypher.js
@@ -1,10 +1,7 @@
 function checkInput() {
   var a = document.getElementById("stage4Input");
   if (a.value == "izgeqg nexpnzrk" || a.value == "IZGEQG NEXPNZRK") {
-    document.getElementById("err").innerHTML =
-      "Congratulations, Proceed to the next Stage";
-    document.getElementById("proceedToStage5").innerHTML = "Stage 5";
-    document.getElementById("proceedToStage5").classList.add = "btn-stage-2";
+    location.href = "/cypher-3331/final.html";
   } else {
     document.getElementById("err").innerHTML = "Wrong";
   }
