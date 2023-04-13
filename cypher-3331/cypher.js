ab266c48615df00447916f9b1190521224306109
Subhadip De <subhadipde.habra@gmail.com>
2023-04-13 22:36:33 +0530

    done cypher 3331

--- cypher-3331/cypher.js
+++ cypher-3331/cypher.js
@@ -9,3 +9,26 @@ function checkInput() {
     document.getElementById("err").innerHTML = "Wrong";
   }
 }
+
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+document.onkeydown = (e) => {
+  if (e.key == 123) {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "I") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "C") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.shiftKey && e.key == "J") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.key == "U") {
+    e.preventDefault();
+  }
+  if (e.ctrlKey && e.key == "F") {
+    e.preventDefault();
+  }
+};
